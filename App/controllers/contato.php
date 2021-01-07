9c57bda7076836a0539dd2277dc72acfa10307a7
Ricardo Moreira <calditos1988@outlook.com>
2021-01-07 12:30:08 -0300

    criação das rotas

---
+++ App/controllers/contato.php
@@ -0,0 +1,16 @@
+<?php
+
+class Contato {
+
+    public function index() {
+        echo "index do contato";
+    }
+
+    public function email() {
+        echo "ricardo ferreira";
+    }
+
+    public function telefone() {
+        echo "12 981255454";
+    }
+}
